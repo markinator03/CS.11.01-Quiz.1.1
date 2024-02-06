6253baf790986c8cf5132c12b63e7c3d123621df
mark <markt2026@student.cis.edu.hk>
2024-02-06 11:16:43 +0800

    Unit 1 test

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-Scanner scan;
+Scanner scanner = new Scanner(System.in);
 int age;
 String firstName;
 String favFood;
 System.out.println("Enter your age");
-scan.nextInt() = age;
-
+age = scanner.nextInt();
+System.out.println("Enter your first name");
+firstName = scanner.next();
+System.out.println("Enter your favorite food");
+favFood = scanner.next();
+System.out.println("Name: " + firstName + "\n\nAge: " + age + "\n\nFavorite food: " + favFood);
 
 
     }
